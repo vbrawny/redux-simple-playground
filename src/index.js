12923040ce32eecdf653757bb55b6665d4ad97f3
venky <vbrawny@users.noreply.github.com>
2021-07-18 10:47:52 +0100

    illustrated compose api

--- src/index.js
+++ src/index.js
@@ -6,3 +6,20 @@ import {
   applyMiddleware,
   bindActionCreators
 } from "redux";
+
+const makeLouder = (string) => string.toUpperCase();
+
+const repeatThreeTimes = (string) => string.repeat(3);
+
+const bolden = (string) => string.bold();
+
+// const makeLouderRepeatThreeTimesAndEmbolden = (string) =>
+//   bolden(repeatThreeTimes(makeLouder(string)));
+
+const makeLouderRepeatThreeTimesAndEmbolden = compose(
+  bolden,
+  repeatThreeTimes,
+  makeLouder
+);
+
+console.log(makeLouderRepeatThreeTimesAndEmbolden("hello"));
